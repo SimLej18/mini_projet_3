0095a7769187ee0621d285551bd898113f7786d1
SimLej18 <44818176+SimLej18@users.noreply.github.com>
2018-12-03 11:32:49 +0100

    Pylint enhancement

--- get_prob_from_theme.py
+++ get_prob_from_theme.py
@@ -1,5 +1,5 @@
 def get_prob_from_theme(theme_list, text_list):
-    """Return pourcentage for links between the text and the theme.
+    """Return percentage for links between the text and the theme.
     Parameters
     ----------
     theme_list: list of theme checked (list)
@@ -10,25 +10,26 @@ def get_prob_from_theme(theme_list, text_list):
     prob_1: the probabilities of being in theme (float)
 
     """
-    sibling_word=[]
-    #check word by word
+
+    sibling_word = []
+
+    # check word by word
     for word_theme in theme_list:
         for word_text in text_list:
-            if word_text == word_text:
+            if word_theme == word_text:
                 sibling_word.append(word_text)
     
-    #compute probabilities
+    # compute probabilities
     prob_1 = 1
     for prob in sibling_word:
-        #multiplicate the probabilities by the pourcentage of appeared word
+        # multiplicate the probabilities by the pourcentage of appeared word
         prob_1 *= theme_list[prob][0]
     for prob in theme_list:
-        #multiplicate the probabilities by the pourcentage of unappeared word 
+        # multiplicate the probabilities by the pourcentage of unappeared word
         if not check_sibling(prob, sibling_word):
             prob_1 *= theme_list[prob][1]
     return prob_1  
     
-            
 
 def check_sibling(check_word, sibling_word):
     """Return True if the word is in both list. False otherwise
@@ -46,5 +47,5 @@ def check_sibling(check_word, sibling_word):
     for test_word in sibling_word:
         if check_word == test_word:
             return True
-    #after checked the whole list, and there is no sibling 
+    # after checked the whole list, and there is no sibling
     return False
