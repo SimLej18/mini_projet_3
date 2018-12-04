d347662d75f6b660bfc76be505b56705d10ee0d1
SimLej18 <44818176+SimLej18@users.noreply.github.com>
2018-12-04 16:51:21 +0100

    Add files via upload

--- file_in_a_flash_gr_12.py
+++ file_in_a_flash_gr_12.py
@@ -1,5 +1,7 @@
 import learn
 import sort
+from check_accuracy import *
+
 
 def files_in_a_flash(path):
     """
@@ -7,5 +9,10 @@ def files_in_a_flash(path):
     -----------
     path: the path to the emplacement of the archives file in memory
     """
-    probabilities = learn.learn(path)
-    sort.sort(probabilities, path)
+    # probabilities = learn.learn(path+'/sorted')
+    # sort.sort(probabilities, path)
+
+
+files_in_a_flash('/Users/simonlejoly/Documents local/Project/Archives/archive_2')
+accuracy = check_accuracy('/Users/simonlejoly/Documents local/Project/Archives')
+print('The sort AI has a %.3f %% of accuracy' % accuracy)
