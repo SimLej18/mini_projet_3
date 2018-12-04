ef6a9edfe0978fc01fa0db23a5b8ae31164ca589
guillaume-nizet <guillaume.nizet@student.unamur.be>
2018-12-04 12:22:38 +0100

    Add files via upload

--- learn.py
+++ learn.py
@@ -15,27 +15,27 @@ def get_words_in_file(path):
     file = open(path, 'r')
 
     characters_allowed = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    blacklist = ['the','be','are','of','and','a','in','that','have','s','i','it','but','etc','to','for','not','on','with','has','he','as','you','do','at','this','his','by','from','they','we','say','her','she','on','an','will','my','one','all','would','there','their','what','so','up','out','if','about','who','get','which','me','when','make','can','like','no','just','him','know','take','into','your','good','same','could','them','see','other','than','then','now','look','only','come','its','over','think','also','back','after','use','two','how','our','first','well','way','even','new','want','because','any','these','give','day','most','us','few','bye','regards','mr','ms','is','or','dt','t','q','why','am','p','had','some','ve','re','thanks','once','','']
+    blacklist = [' ','the','be','are','of','and','a','in','that','have','s','i','it','but','etc','to','for','not','on','with','has','he','as','you','do','at','this','his','by','from','they','we','say','her','she','on','an','will','my','one','all','would','there','their','what','so','up','out','if','about','who','get','which','me','when','make','can','like','no','just','him','know','take','into','your','good','same','could','them','see','other','than','then','now','look','only','come','its','over','think','also','back','after','use','two','how','our','first','well','way','even','new','want','because','any','these','give','day','most','us','few','bye','regards','mr','ms','is','or','dt','t','q','why','am','p','had','some','ve','re','thanks','once','','']
 
     list_words = []
 
-    counter = 1
+    message_with_spaces = ''
 
-    message = file.read()
+    message = file.read().lower()
 
-    while counter != len(message):
-        if message[counter - 1] not in characters_allowed and message[counter] in characters_allowed: # A word is starting
-            word = ''
-            while message[counter] in characters_allowed: # While the word is not "finished"
-                word += message[counter]
-                counter += 1
+    for character in message:
+        if character not in characters_allowed:
+            message_with_spaces += ' '
+        else:
+            message_with_spaces += character
 
-            word = word.lower()
+    list_words = str.split(message_with_spaces, ' ')
+    
+    for word in list_words:
+        if word in blacklist:
+            list_words.remove(word)
 
-            if word not in blacklist and len(word) > 2:
-                list_words.append(word) # The word is added to the list
-
-        counter += 1
+    list_words = list(filter(None, list_words)) # Deletes empty strings
 
     file.close()
 
@@ -100,7 +100,7 @@ def get_occurence(path):
         words_occurence.update({word: ("%.5f" % appearance, "%.5f" % (1 - appearance))})
 
     return words_occurence
-    
+
 
 def learn(path):
     """ For every theme in a directory, ...
@@ -125,3 +125,6 @@ def learn(path):
         probabilities.update({directory : dict_probabilities})
         
     return probabilities
+
+dico = learn('E:/archive_1/sorted')
+print(dico)
