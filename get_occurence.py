a82139ad6995cbe8a0f92851d8549edb81586130
guillaume-nizet <guillaume.nizet@student.unamur.be>
2018-12-04 10:48:56 +0100

    Update get_occurence.py

--- get_occurence.py
+++ get_occurence.py
@@ -1,32 +1,35 @@
 def get_occurence(theme):
     """Determines how often a word appears in the texts of each theme.
-
     Parameters
     ----------
     theme: The themes checked (str)
-
     Returns
     -------
     words_occurence: The probability that a word is found in the texts of a theme (dic)
-
     """
 
     words_occurence = {}
-    word_list = get_word_theme(theme)
-    text_list = os.listdir("./sorted/" + theme)
+    word_list = get_words_theme(theme)
+    text_list = os.listdir("D:/Programmation/UNamur/Mini-projet 3/Archives/archive_1/sorted/" + theme)
+
+    words = 0
+
+    dico_words = {}
+
+    number_texts = 0
+
+    for text in text_list:
+        dico_words.update({text : get_words_in_file("D:/Programmation/UNamur/Mini-projet 3/Archives/archive_1/sorted/" + theme + "/" + text)})
+        number_texts += 1
 
     for word in word_list:
-        number_texts = 0
-        word_found = 0
-        for text in text_list:
-            number_texts += 1
-            if word in get_words_in_file("./sorted/" + theme + "/" + text):
-                word_found += 1
-            else:
-                word_found += 0
+        words += 1 
+        words_found = 0
+        for text in text_list:       
+            if word in dico_words[text]:
+                words_found += 1
 
-        appearance = word_found / number_texts
-        no_appearance = 1 - appearance
-        words_occurence.update({word: ("%.1f" % appearance, "%.1f" % no_appearance)})
+        appearance = words_found / number_texts
+        words_occurence.update({word: ("%.5f" % appearance, "%.5f" % (1 - appearance))})
 
     return words_occurence
