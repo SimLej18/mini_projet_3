c9a3742eb18cc290f333b811f6904deb420b6211
guillaume-nizet <guillaume.nizet@student.unamur.be>
2018-12-05 10:16:35 +0100

    Still needs to be merged with pylint

--- learn - functional.py
+++ learn - functional.py
@@ -2,11 +2,9 @@ import os
 
 def get_words_in_file(path):
     """Returns the list of words contained in a file. 
-
     Parameters:
     ----------
     path: the path of the analyzed file (str)
-
     Returns:
     -------
     list_words: the list of words contained in the file (list)
@@ -14,8 +12,8 @@ def get_words_in_file(path):
 
     file = open(path, 'r', encoding="ISO-8859-1")
 
-    characters_allowed = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    blacklist = [' ','the','be','are','of','and','a','in','that','have','s','i','it','but','etc','to','for','not','on','with','has','he','as','you','do','at','this','his','by','from','they','we','say','her','she','on','an','will','my','one','all','would','there','their','what','so','up','out','if','about','who','get','which','me','when','make','can','like','no','just','him','know','take','into','your','good','same','could','them','see','other','than','then','now','look','only','come','its','over','think','also','back','after','use','two','how','our','first','well','way','even','new','want','because','any','these','give','day','most','us','few','bye','regards','mr','ms','is','or','dt','t','q','why','am','p','had','some','ve','re','thanks','once','','']
+    characters_allowed = 'abcdefghijklmnopqrstuvwxyz'
+    blacklist = ['without','com','subject','lines','the','be','are','of','and','a','in','that','have','s','i','it','but','etc','to','for','not','on','with','has','he','as','you','do','at','this','his','by','from','they','we','say','her','she','on','an','will','my','one','all','would','there','their','what','so','up','out','if','about','who','get','which','me','when','make','can','like','no','just','him','know','take','into','your','good','same','could','them','see','other','than','then','now','look','only','come','its','over','think','also','back','after','use','two','how','our','first','well','way','even','new','want','because','any','these','give','day','most','us','few','bye','regards','mr','ms','is','or','dt','t','q','why','am','p','had','some','ve','re','thanks','once','','']
 
     list_words = []
 
@@ -29,40 +27,23 @@ def get_words_in_file(path):
         else:
             message_with_spaces += character
 
+
     list_words = str.split(message_with_spaces, ' ')
     
-    for word in list_words:
-        if word in blacklist:
-            list_words.remove(word)
 
-    list_words = list(filter(None, list_words))  # Deletes empty strings
+    list_words = list(filter(None, list_words))  # Delete empty strings
+    list_words = list(set(list_words))  # Remove duplicate words
+
+    cleared_list_words = []
+
+    for word in list_words:
+        if word not in blacklist:
+            cleared_list_words.append(word)
+
+    list_words = cleared_list_words
 
     file.close()
 
-    return list_words
-
-
-def get_words_theme(path):
-    """Returns all the words of a theme
-    
-    Parameters:
-    ----------
-    path : the path to the theme (str)
-
-    Returns:
-    --------
-    list_words : list of all the words of the theme (list)
-
-    """
-
-    list_words = []
-    text_list = os.listdir(path)
-    
-    for text in text_list:
-        list_words.extend(get_words_in_file(path + "/" + text))
-
-    list_words = list(set(list_words))  # Remove duplicate words
-    
     return list_words
 
 
@@ -78,37 +59,31 @@ def get_occurence(path):
     words_occurence: The probability that a word is found in the texts of a theme (dict)
     """
 
-    words_occurence = {}
-    word_list = get_words_theme(path)
     text_list = os.listdir(path)
 
-    dico_words = {}
-
-    number_texts = 0
+    words_occurence = {}
 
     for text in text_list:
-        dico_words.update({text : get_words_in_file(path + "/" + text)})
-        number_texts += 1
+        for word in get_words_in_file(path + '/' + text):
+            if word in words_occurence:
+                words_occurence[word] += 1
+            else:
+                words_occurence[word] = 1
 
-    for word in word_list: 
-        words_found = 0
-        for text in text_list:       
-            if word in dico_words[text]:
-                words_found += 1
+    for element in words_occurence:
+        words_occurence[element] /= len(text_list)
 
-        appearance = words_found / number_texts
-        words_occurence.update({word: (appearance, 1 - appearance)})
+        # Put the elements in the form (Probability, Anti-probability)
+        words_occurence[element] = (words_occurence[element], 1 - words_occurence[element])
 
     return words_occurence
 
 
 def learn(path):
     """ For every theme in a directory, ...
-
         Parameters:
         ----------
         path: the path to the directory containing the themes (str)
-
         Returns:
         -------
         probabilities: dicionary of which 
